588176721d87050c59b00d776f84f24fa03157b6
Alexandra Grecu <elena.grecu@s.unibuc.ro>
2020-12-29 22:53:43 +0200

    Test

--- OnlineShop/Controllers/WeatherForecastController.cs
+++ OnlineShop/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace OnlineShop.Controllers
-{
+{ // initial commit
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
