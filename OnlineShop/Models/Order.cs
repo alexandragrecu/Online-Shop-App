830e87691a35659834b937ede0be2d42a6039b20
Alexandra Grecu <elena.grecu@s.unibuc.ro>
2020-12-30 19:05:14 +0200

    Add Order and Product models.

---
+++ OnlineShop/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class Order
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public int OrderNumber { get; set; }   
+
+    }
+}
---
+++ OnlineShop/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string Name  { get; set; }
+        public float Price  { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public int OrderId { get; set; }
+    }
+}
